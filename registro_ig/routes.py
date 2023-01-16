8874132c3fc787660a0006d7ce6c5d1e16d3a2f4
ivanes79 <ivanes666@hotmail.es>
2023-01-16 18:53:13 +0100

    funcion alta,funcion delete y registrar en BD

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,10 +1,24 @@
-from registro_ig import *
-from flask import render_template
+from registro_ig import app
+from flask import render_template,request,redirect,url_for
+from registro_ig.models import select_all,insert,select_by,delete_by
+from datetime import date
 
-
+def validateForm(requestForm):
+    hoy = date.today().isoformat()
+    errores=[]
+    if requestForm['date'] > hoy:
+        errores.append("fecha invalida: La fecha introducida es a futuro")
+    if requestForm['concept'] == "":
+        errores.append("concepto vacio: Introduce un concepto para el registro")
+    if requestForm['quantity'] == "" or float(requestForm['quantity']) == 0.0:
+        errores.append("cantidad vacio o cero: Introduce una cantidad positiva o negativa")   
+    return errores
 
 @app.route("/")
 def index():
+
+
+    registros = select_all()
 
     datos_mov=[
         {"id":1,"date":"2022-01-01","concept":"Sueldo","quantity":1500},
@@ -12,4 +26,33 @@ def index():
         {"id":3,"date":"2022-01-06","concept":"Almuerzo Reyes","quantity":-100},
     ]
     #return "Esto funciona, como mola flask!!!"
-    return render_template("index.html",pageTitle="Todos",data=datos_mov)
+    return render_template("index.html",pageTitle="Todos",data=registros)
+
+
+@app.route("/new",methods=["GET","POST"])
+def create():
+
+    if request.method == "GET":
+        return render_template("new.html",dataForm=None)
+    else:
+        #como recibo los datos del formulario
+        errores = validateForm(request.form)
+
+        if errores:
+            return render_template("new.html",msgError=errores,dataForm=request.form)
+        else:
+            insert([ request.form['date'],
+                     request.form['concept'],
+                     request.form['quantity']  ])
+            
+            return redirect(url_for('index'))
+
+@app.route("/delete/<int:id>",methods=["GET","POST"]) 
+def remove(id):
+
+    if request.method == "GET":
+        resultado = select_by(id)
+        return render_template("delete.html",data=resultado)   
+    else:
+        delete_by(id)
+        return redirect(url_for('index'))
